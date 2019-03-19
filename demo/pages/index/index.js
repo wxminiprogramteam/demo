e966ac6697a62a51f7dce2eacf2027b5fd22e28b
manzuo <2395722107@qq.com>
2019-03-19 10:28:00 +0800

    图标替换

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -7,31 +7,31 @@ Page({
     list:[
       {
         id: "1",  
-        imgUrl: "../../images/orientation.png",
+        imgUrl: "../../images/xiaochengxu.png",
         caption: "高校小程序大赛讲座",
         content: "了解小程序大赛相关事宜",
       },
       {
         id: "2",
-        imgUrl: "../../images/pencil.png",
+        imgUrl: "../../images/qingxie.jpg",
         caption: "志愿梦·中国梦，共筑青春梦",
         content: "志愿者通识培训",
       },
       {
         id: "3",
-        imgUrl: "../../images/school_bus.png",
+        imgUrl: "../../images/huawei .png",
         caption: "枪林弹雨中成长",
         content: "华为全国巡回公益讲座",
       },
       {
         id: "4",
-        imgUrl: "../../images/set_square.png",
+        imgUrl: "../../images/xue.jpg",
         caption: "学而思网校宣讲会",
         content: "了解学而思网校",
       },
       {
         id: "5",
-        imgUrl: "../../images/studies.png",
+        imgUrl: "../../images/shu.jpg",
         caption: "曙光基金宣讲会",
         content: "助学筑梦，曙光基金助你前行",
       },
